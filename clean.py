052cc5d5ee4fdfc2247169ec0e44485815e2956c
Dương Anh Kiệt <43516744+ffyyytt@users.noreply.github.com>
2025-02-01 23:42:38 +0100

    Google tag

--- clean.py
+++ clean.py
@@ -1,9 +1,21 @@
 import glob
+from tqdm import *
 
 files = glob.glob("*.html")
-for file in files:
+for file in tqdm(files):
     with open(file, "rb") as f:
         data = f.read()
     data = data.replace(b'<meta name="robots" content="noindex">', b"")
+    data = data.replace(b'<head>', b'''<head> 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HEFX4ERE73"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HEFX4ERE73');
+</script>
+''')
     with open(file, "wb") as f:
         data = f.write(data)
